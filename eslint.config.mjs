cc5be029e1c5fcb1d9ac8871ee93175d4d6e7c26
John Gozde <john@gozde.ca>
2024-11-27 09:34:11 -0700

    globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,16 +9,6 @@ import awesomeCodeStyle, { configs } from '@awesome-code-style/eslint-config';
 import notice from 'eslint-plugin-notice';
 import globals from 'globals';
 
-const HEADER_TEMPLATE = `
-/*
- * Copyright (c) Imply Data, Inc. All rights reserved.
- *
- * This software is the confidential and proprietary information
- * of Imply Data, Inc.
- */
-
-`.trimStart();
-
 const TYPESCRIPT_FILES = ['**/*.ts', '**/*.tsx'];
 
 export default [
